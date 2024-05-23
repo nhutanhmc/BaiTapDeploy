371d5c8febe27739bcc69e4b43259c8bce1ca16a
kiet-444 <kietcao4444@gmail.com>
2024-05-23 10:49:46 +0700

    model fail

--- model/paymentModel.js
+++ model/paymentModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 
-const PaymentSchema = new mongoose.Schema({
+const PaymentSchema = new Schema({
     cash: { type: Boolean },
     bank: { type: String }
 });
